47734586a37cc278dd028c300d24aa4a727cbda5
moazTello <tello.m.m.t.95@gmail.com>
2024-09-07 17:07:34 +0300

    404_solve

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
-// import { useStore } from "../context/useStore";
+import { useStore } from "../context/useStore";
 const Layout = () => {
-  // const { logedin } = useStore();
+  const { logedin } = useStore();
   return (
     <div>
-      {/* {logedin &&  */}
-      <Navbar />
-      {/* // } */}
+      {logedin && 
+        <Navbar />
+      }
       <Outlet />
     </div>
   );
